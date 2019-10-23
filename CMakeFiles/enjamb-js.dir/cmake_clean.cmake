a17286f3a70d20f545fb52947409f5195205d1ff
TartanLlama <TartanLlama@users.noreply.github.com>
2019-10-23 13:24:17 +0000

    Automated deployment: Wed Oct 23 13:24:17 UTC 2019 8142cdda0177c01a740fd4dfe65cad2bda8719fa

---
+++ CMakeFiles/enjamb-js.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../../docs/enjamb.js"
+  "../../../docs/enjamb.pdb"
+  "CMakeFiles/enjamb-js.dir/entrypoint.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/enjamb-js.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
